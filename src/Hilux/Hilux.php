cc441aed5a0f6550597ce948d9e374b2145e7ba6
elarity <42677833@qq.com>
2020-09-27 19:57:57 +0800

    init PHP sdk

---
+++ src/Hilux/Hilux.php
@@ -0,0 +1,49 @@
+<?php
+namespace Hilux;
+use Hilux\Lib\Murmurhash;
+use Hilux\Lib\Parser;
+
+class Hilux{
+
+    private $iBucketMax = 1000;
+    //private $sToggleFile = "";
+    public function __construct() {
+        //$this->sToggleFile = "./data.json";
+    }
+
+    /*
+     * @param : $sToggleName | 名称
+     * @param : $aParam      | array数组
+     * @return : bool
+     * */
+    public function toggle($sToggleName, $aParam) {
+        $bRet = true;
+        if (empty($aParam['key'])) {
+            return false;
+        }
+
+        $sKeyName = $aParam['key'];
+        $iHashNum = Murmurhash::hash3_int($sKeyName);
+
+        $aRules = Parser::getRules($sKeyName);
+        if (!$aRules) {
+            return false;
+        }
+        //print_r($aRules);
+
+        $iBucketIndex = $iHashNum % $this->iBucketMax;
+        $aBucketInfo  = $aRules['bucket_info'];
+        $iStartBucketIndex = $aBucketInfo['begin'];
+        $iBucketRate       = $aBucketInfo['rate'];
+
+        if ($iBucketRate == $this->iBucketMax) {
+            return true;
+        }
+
+        $iEndBukectIndex = $iStartBucketIndex + $iBucketRate;
+        
+
+        return $bRet;
+    }
+
+}
